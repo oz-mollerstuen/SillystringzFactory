ecbc4c1f0364afcb50a7ef87e85059bd3463ab9f
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2023-01-06 10:52:33 -0800

    Add new build for sillystringz factory

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+
+namespace Factory.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+    
+    [HttpGet("/")]
+    public ActionResult Index() {
+      ViewBag.Machine = new List<Machine>(_db.Machines);
+      ViewBag.Engineer = new List<Engineer>(_db.Engineer);
+      
+      return View(ViewBag.Machine);
+    }
+  }
+}
---
+++ Models/Engineer.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<MachineEngineer>();
+    }
+
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<MachineEngineer> JoinEntities { get; set; }
+  }
+}
