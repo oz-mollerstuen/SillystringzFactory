b1249978fda1377fed942900aeb204c5be709327
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2023-01-06 16:05:02 -0800

    Update controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
